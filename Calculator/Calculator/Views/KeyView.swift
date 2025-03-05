4efbc926863277dc8b90dbd0527e503f81e9294b
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-05 18:51:33 +0530

    create basic logic of calculator

--- Calculator/Calculator/Views/KeyView.swift
+++ Calculator/Calculator/Views/KeyView.swift
@@ -74,7 +74,51 @@ struct KeyView: View {
     }
     
     func didTap(button: Keys) {
-        print("TODO: implement button click")
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch currentOperation {
+                case .add:
+                    self.value = "\(runningValue + currentValue)"
+                case .subtract:
+                    self.value = "\(runningValue - currentValue)"
+                case .multiply:
+                    self.value = "\(runningValue * currentValue)"
+                case .divide:
+                    self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+        case .negative, .decimal, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                self.value = number
+            } else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
     }
 }
 
